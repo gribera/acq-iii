97a185d4ebe0d5f019eca49abafd227ff0fdde55
Gonzalo Ribera <gonzalo.ribera@gmail.com>
2025-11-12 10:59:03 -0300

    implementa manejo de memorias

--- config.py
+++ config.py
@@ -2,3 +2,10 @@
 COMANDO_INICIO = "\x1b"                         # Comando que da inicio a la recepción de comandos
 COMANDO_FIN = "\x0d"                            # Comando que da fin a la recepción de comandos
 TIMEOUT_COMANDOS = 5                            # Timeout para la recepción de comandos
+
+# EEPROM
+E2 = {
+  "E1": 0x50,
+  "E2": 0x54,
+  "RTC": 0x68
+}
---
+++ core/memory.py
@@ -0,0 +1,133 @@
+import time
+from utils.hardware import get_i2c
+import digitalio
+import board
+
+from config import E2
+
+class MemoryService:
+    def __init__(self, write_protect_pin=board.D34):
+        self.i2c = get_i2c()
+        self.wp = digitalio.DigitalInOut(write_protect_pin)
+        self.wp.direction = digitalio.Direction.OUTPUT
+        self._enable_write_protect()
+
+    def write_string(self, e2: int, mem_addr: int, text: str):
+        """
+        Escribe un string en la memoria indicada.
+
+        Args:
+            e2 (str): Dirección del dispositivo ("E1", "E2" o "RTC").
+            mem_addr (int): Dirección de inicio donde se escribirá el string.
+            text (str): String a escribir.
+
+        Returns:
+            None
+        """
+        self.address = e2
+        data = text.encode('ascii')
+
+        if e2 == E2["RTC"]:
+            self._write_bytes_rtc(mem_addr, data)
+        else:
+            self._write_bytes(mem_addr, data)
+
+
+    def read_string(self, e2: int, mem_addr: int, length: int) -> str:
+        """
+        Lee un string desde la memoria indicada.
+
+        Args:
+            e2 (str): Dirección del dispositivo ("E1", "E2" o "RTC").
+            mem_addr (int): Dirección de inicio desde donde se comenzará a leer.
+            length (int): Cantidad de bytes a leer.
+
+        Returns:
+            str: String leído desde la memoria.
+        """
+        self.address = e2
+
+        if e2 == E2["RTC"]:
+            data =  self._read_bytes_rtc(mem_addr, length)
+        else:
+            data = self._read_bytes(mem_addr, length)
+
+        return data.decode('ascii')
+
+    def _disable_write_protect(self):
+        self.wp.value = False
+
+    def _enable_write_protect(self):
+        self.wp.value = True
+
+    def _write_bytes(self, mem_addr: int, data: bytes):
+        addr_high = (mem_addr >> 8) & 0xFF
+        addr_low = mem_addr & 0xFF
+        payload = bytes([addr_high, addr_low]) + data
+        self._disable_write_protect()
+
+        while not self.i2c.try_lock():
+            pass
+        try:
+            self.i2c.writeto(self.address, payload)
+        finally:
+            self.i2c.unlock()
+
+        self._enable_write_protect()
+        time.sleep(0.1)
+
+    def _read_bytes(self, mem_addr: int, length: int) -> bytes:
+        addr_high = (mem_addr >> 8) & 0xFF
+        addr_low = mem_addr & 0xFF
+        out_buf = bytes([addr_high, addr_low])
+        in_buf = bytearray(length)
+
+        while not self.i2c.try_lock():
+            pass
+        try:
+            self.i2c.writeto_then_readfrom(self.address, out_buf, in_buf)
+        finally:
+            self.i2c.unlock()
+
+        return bytes(in_buf)
+
+    def close(self):
+        if self.wp:
+            self.wp.deinit()
+            self.wp = None
+
+    def __del__(self):
+        self.close()
+
+
+    def _write_bytes_rtc(self, start_addr: int, data: bytes):
+        if not (0x08 <= start_addr <= 0x3F):
+            raise ValueError("Dirección fuera de rango (0x08–0x3F)")
+        if start_addr + len(data) > 0x40:
+            raise ValueError("Excede el tamaño de la RAM (56 bytes)")
+
+        while not self.i2c.try_lock():
+            pass
+        try:
+            # a diferencia de las otras e2 se guarda byte a byte
+            for i, b in enumerate(data):
+                self.i2c.writeto(self.address, bytes([start_addr + i, b]))
+                time.sleep(0.005)
+        finally:
+            self.i2c.unlock()
+
+    def _read_bytes_rtc(self, start_addr: int, length: int) -> bytes:
+        if not (0x08 <= start_addr <= 0x3F):
+            raise ValueError("Dirección fuera de rango (0x08–0x3F)")
+        if start_addr + length > 0x40:
+            raise ValueError("Excede el tamaño de la RAM (56 bytes)")
+
+        out_buf = bytes([start_addr])
+        in_buf = bytearray(length)
+        while not self.i2c.try_lock():
+            pass
+        try:
+            self.i2c.writeto_then_readfrom(self.address, out_buf, in_buf)
+        finally:
+            self.i2c.unlock()
+        return bytes(in_buf)
